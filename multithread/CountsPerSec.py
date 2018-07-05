edb4d2ae633a36428ee829479375b85c9b2508cf
najam <nsyed88@gmail.com>
2018-07-05 18:14:13 -0400

    corrected typo

--- multithread/CountsPerSec.py
+++ multithread/CountsPerSec.py
@@ -9,15 +9,15 @@ class CountsPerSec:
 
     def __init__(self):
         self._start_time = None
-        self._num_occurences = 0
+        self._num_occurrences = 0
 
     def start(self):
         self._start_time = datetime.now()
         return self
 
     def increment(self):
-        self._num_occurences += 1
+        self._num_occurrences += 1
 
     def countsPerSec(self):
         elapsed_time = (datetime.now() - self._start_time).total_seconds()
-        return self._num_occurences / elapsed_time
+        return self._num_occurrences / elapsed_time
